adc27392f03ef8fa07d4bc727d06e79c88a7227b
Makukumba <evgrafborezov@yandex.ru>
2022-04-24 14:58:53 +0300

    1

--- src/Managers/FileManager.java
+++ src/Managers/FileManager.java
@@ -1,8 +1,8 @@
 package Managers;
-
+import com.google.gson.Gson;
 public class FileManager {
     private String E;
-
+    private Gson gson = new Gson();
     public FileManager(String e) {
         this.E = e;
     }
