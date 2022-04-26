2da6e7e77dab71233fd7859ff4dee9f86f41ffc8
Makukumba <evgrafborezov@yandex.ru>
2022-04-26 16:17:36 +0300

    nauchilis chitat' json

--- src/Managers/Parser.java
+++ src/Managers/Parser.java
@@ -1,19 +1,30 @@
 package Managers;
 
+import Drago.Dragon;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.io.FileReader;
+import java.lang.reflect.Type;
+import java.util.Scanner;
+import java.util.TreeSet;
 
 public class Parser {
     public ParseHelper parse(){
         Gson gson = new Gson();
+        try(FileReader reader = new FileReader("C:\\Users\\Public\\lab5\\dragon.json")){
+            Scanner scan = new Scanner(reader);
+            ParseHelper parseHelper = new ParseHelper();
 
-        try (FileReader reader = new FileReader("C:\\Users\\Public\\lab5\\src\\dragon.json")){
-            ParseHelper parseHelper = gson.fromJson(reader, ParseHelper.class);
+            TreeSet<Dragon> collection;
+            Type collectionType = new TypeToken<TreeSet<Dragon>>() {}.getType();
+            collection = gson.fromJson(scan.nextLine().trim(), collectionType);
+            parseHelper.setDragon(collection);
 
             return parseHelper;
-        } catch (Exception e){
-            System.out.println("Parsing error" + e.toString());
         }
-        return null;
-    }}
+        catch (Exception e ){
+            System.out.println("Parsing error"+e.toString());
+        }
+        return new ParseHelper(); }
+}
